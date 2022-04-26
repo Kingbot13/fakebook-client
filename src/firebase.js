c6c1bb4254896068861be555dd14e67d5ae3f36a
Dylan King <dwkblue51@yahoo.com>
2022-04-26 10:02:14 -0500

    setup firebase

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAKxM3t6InarqvRbw4K44pG086MyEjava8",
+  authDomain: "fakebook-6445f.firebaseapp.com",
+  projectId: "fakebook-6445f",
+  storageBucket: "fakebook-6445f.appspot.com",
+  messagingSenderId: "13658216820",
+  appId: "1:13658216820:web:2a012e80f930227fb1f421"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
