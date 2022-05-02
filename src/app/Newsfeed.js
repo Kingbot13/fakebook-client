15039743dc4b8477627c6989c3b6f0356a52541f
Dylan King <dwkblue51@yahoo.com>
2022-05-02 13:11:18 -0500

    begin setup for fetching posts

---
+++ src/app/Newsfeed.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Newsfeed = () => {
+  return <main></main>;
+};
--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -1,5 +1,29 @@
-import { createApi } from "@reduxjs/toolkit/query/react";
+import { createApi, fakeBaseQuery } from "@reduxjs/toolkit/query/react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../firebase";
+
+const getPostsFromFirebase = async () => {
+  try {
+    let posts = [];
+    const request = await getDocs(collection(db, "posts"));
+    request.forEach((doc) => posts.push({ data: doc.data() }));
+    return { data: posts };
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 export const apiSlice = createApi({
-  // add reducers here
+  baseQuery: fakeBaseQuery(),
+  endpoints: (build) => ({
+    getPosts: build.query({
+      queryFn() {
+        try {
+          getPostsFromFirebase();
+        } catch (err) {
+          console.error(err);
+        }
+      },
+    }),
+  }),
 });
