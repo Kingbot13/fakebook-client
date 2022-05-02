8b88db4450a9e17f865400ffc199e46550f02d39
Dylan King <dwkblue51@yahoo.com>
2022-05-02 08:34:19 -0500

    add headers to SignInPage

--- src/app/SignInPage.js
+++ src/app/SignInPage.js
@@ -4,6 +4,10 @@ import { SignInForm } from "./SignInForm";
 export const SignInPage = () => {
   return (
     <main>
+      <div>
+        <h1>Fakebook</h1>
+        <h2>Connect with friends and the world around you on Facebook.</h2>
+      </div>
       <SignInForm />
     </main>
   );
