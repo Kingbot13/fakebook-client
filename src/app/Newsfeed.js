4bd681b7c2c247d12d705a68f6c436f8feaa1689
Dylan King <dwkblue51@yahoo.com>
2022-05-03 14:24:02 -0500

    conditionally render PostForm

--- src/app/Newsfeed.js
+++ src/app/Newsfeed.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import { PostFormButton } from "../features/posts/PostFormButton";
+import { PostForm } from "../features/posts/PostForm";
 
 export const Newsfeed = () => {
   const [showForm, setShowForm] = useState(false);
@@ -9,6 +10,7 @@ export const Newsfeed = () => {
   }
   return (
     <main>
+      {showForm && <PostForm />}
       <PostFormButton onClick={toggleForm} />
     </main>
   );
