2e4c3019ad4ee9f07e75d12aaa389da86c1414a0
Dylan King <dwkblue51@yahoo.com>
2022-05-03 21:39:33 -0500

    only add new user if user does not already exist

--- src/app/SignInForm.js
+++ src/app/SignInForm.js
@@ -3,16 +3,20 @@ import { auth, db, provider } from "../firebase";
 import { signInWithRedirect } from "firebase/auth";
 import { useNavigate as navigate } from "react-router-dom";
 import { collection, setDoc } from "firebase/firestore";
+import { useGetUsersQuery } from "../features/api/apiSlice";
 
 export const SignInForm = () => {
   const googleSignIn = async () => {
     try {
+      const { data: users, isError } = useGetUsersQuery();
       await signInWithRedirect(auth, provider);
       const user = auth.currentUser;
-      await setDoc(collection(db, "users", `${user.uid}`), {
-        name: user.displayName,
-        photo: user.photoURL,
-      });
+      if (!users.includes(user.id)) {
+        await setDoc(collection(db, "users", `${user.uid}`), {
+          name: user.displayName,
+          photo: user.photoURL,
+        });
+      }
       // send user to homepage with `navigate`
       navigate("newsfeed");
     } catch (err) {
