3a1d7ac490578a6caea05af05ba7228f219a55f5
Dylan King <dwkblue51@yahoo.com>
2022-05-03 21:05:45 -0500

    add user info to user collection on firebase

--- src/app/SignInForm.js
+++ src/app/SignInForm.js
@@ -1,14 +1,20 @@
 import React from "react";
-import { auth, provider } from "../firebase";
+import { auth, db, provider } from "../firebase";
 import { signInWithRedirect } from "firebase/auth";
 import { useNavigate as navigate } from "react-router-dom";
+import { collection, setDoc } from "firebase/firestore";
 
 export const SignInForm = () => {
   const googleSignIn = async () => {
     try {
       await signInWithRedirect(auth, provider);
+      const user = auth.currentUser;
+      await setDoc(collection(db, "users", `${user.uid}`), {
+        name: user.displayName,
+        photo: user.photoURL,
+      });
       // send user to homepage with `navigate`
-      navigate('newsfeed');
+      navigate("newsfeed");
     } catch (err) {
       console.error(err);
     }
