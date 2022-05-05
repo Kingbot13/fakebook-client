353a99dde1887f32c5262241a5897f27c5b7d5db
Dylan King <dwkblue51@yahoo.com>
2022-05-05 10:02:10 -0500

    flesh out Post component

--- src/features/posts/Post.js
+++ src/features/posts/Post.js
@@ -1,5 +1,16 @@
 import React from "react";
 
-export const Post = () => {
-  return <div></div>;
+export const Post = ({ name, content, photo }) => {
+  return (
+    <div>
+      <div>
+        <div>
+          <img src={photo} alt="" />
+        </div>
+        <div>{name}</div>
+        <div>...</div>
+      </div>
+      <p>{content}</p>
+    </div>
+  );
 };
