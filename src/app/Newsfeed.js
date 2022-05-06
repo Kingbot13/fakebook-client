706294a8b0ebcea1cc7ce8ade84a6ccdd75be81d
Dylan King <dwkblue51@yahoo.com>
2022-05-06 09:44:12 -0500

    flesh out PostsList and import into Newsfeed

--- src/app/Newsfeed.js
+++ src/app/Newsfeed.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import { PostFormButton } from "../features/posts/PostFormButton";
 import { PostForm } from "../features/posts/PostForm";
+import { PostList } from "../features/posts/PostList";
 
 export const Newsfeed = () => {
   const [showForm, setShowForm] = useState(false);
@@ -12,6 +13,7 @@ export const Newsfeed = () => {
     <main>
       {showForm && <PostForm />}
       <PostFormButton onClick={toggleForm} />
+      <PostList />
     </main>
   );
 };
--- src/features/posts/PostList.js
+++ src/features/posts/PostList.js
@@ -1,6 +1,17 @@
 import React from "react";
 import { Post } from "./Post";
+import { useGetPostsQuery } from "../api/apiSlice";
 
 export const PostList = () => {
-  return <div></div>;
+  const {data: posts} = useGetPostsQuery();
+
+  
+
+  return (
+    <div>
+      {posts ? posts.map(post => <Post key={post.id} name={post.name} content={post.content} photo={post.photo} />)
+      :
+      "Be the first to write a post!"}
+    </div>
+  );
 };
