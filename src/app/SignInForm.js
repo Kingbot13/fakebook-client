c8c204a62fa6b24e8adcd46b0ae9f09c1b502bff
Dylan King <dwkblue51@yahoo.com>
2022-05-09 13:18:45 -0500

    user gets redirected to newsfeed page after successful sign in

--- src/app/SignInForm.js
+++ src/app/SignInForm.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { auth, db, provider } from "../firebase";
-import { signInWithRedirect } from "firebase/auth";
+import { getRedirectResult, signInWithRedirect } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import { collection, setDoc } from "firebase/firestore";
 import { useGetUsersQuery } from "../features/api/apiSlice";
@@ -8,24 +8,40 @@ import { useGetUsersQuery } from "../features/api/apiSlice";
 export const SignInForm = () => {
   const { data: users, isError } = useGetUsersQuery();
   const navigate = useNavigate();
+  const user = auth.currentUser;
 
   const googleSignIn = async (e) => {
     try {
       e.preventDefault();
       await signInWithRedirect(auth, provider);
-      const user = auth.currentUser;
       if (!users.includes(user.id)) {
-        await setDoc(collection(db, "users", `${user.uid}`), {
-          name: user.displayName,
-          photo: user.photoURL,
-        });
+        
       }
       // send user to homepage with `navigate`
-      navigate("/newsfeed");
     } catch (err) {
       console.error(err);
     }
   };
+
+  useEffect(() => {
+    getRedirectResult(auth)
+    .then((result) => {
+      console.log(result);
+      addUser();
+      navigate('/newsfeed');
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+    
+  },[]);
+
+  const addUser = async () => {
+    await setDoc(collection(db, "users", `${user.uid}`), {
+      name: user.displayName,
+      photo: user.photoURL,
+    });
+  }
 
   return (
     <form>
