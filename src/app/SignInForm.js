5d93c3ab61386d9ae9c3925b6006f8e6986ecd90
Dylan King <dwkblue51@yahoo.com>
2022-05-10 13:28:27 -0500

    bug fix: change reference to doc from collection in addUser

--- src/app/SignInForm.js
+++ src/app/SignInForm.js
@@ -1,14 +1,13 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { auth, db, provider } from "../firebase";
 import { getRedirectResult, signInWithRedirect } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
-import { collection, setDoc } from "firebase/firestore";
+import { collection, doc, setDoc } from "firebase/firestore";
 import { useGetUsersQuery } from "../features/api/apiSlice";
 
 export const SignInForm = () => {
   const { data: users, isError } = useGetUsersQuery();
   const navigate = useNavigate();
-  const user = auth.currentUser;
 
   const googleSignIn = async (e) => {
     try {
@@ -19,32 +18,34 @@ export const SignInForm = () => {
     }
   };
 
+  const addUser = async (user) => {
+    if (!users.includes(user.id) || !users) {
+    await setDoc(doc(db, "users", `${user.uid}`), {
+      name: user.displayName,
+      photo: user.photoURL,
+    });
+    // }
+  };
+
   useEffect(() => {
     getRedirectResult(auth)
-    .then((result) => {
-      console.log(result);
-      addUser();
-      navigate('/newsfeed');
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-    
-  },[]);
+      .then((result) => {
+        console.log(result);
+        if (result) {
+          const user = auth.currentUser;
 
-  const addUser = async () => {
-    if (!users.includes(user.id)) {
-      await setDoc(collection(db, "users", `${user.uid}`), {
-        name: user.displayName,
-        photo: user.photoURL,
+          addUser(user);
+          navigate("/newsfeed");
+        }
+      })
+      .catch((err) => {
+        console.error(err);
       });
-        
-    }
-  }
+  }, []);
 
   return (
     <form>
-      <button type="button" onClick={e => googleSignIn(e)}>
+      <button type="button" onClick={(e) => googleSignIn(e)}>
         Sign-In With Google
       </button>
     </form>
