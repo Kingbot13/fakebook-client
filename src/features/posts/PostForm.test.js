fff055857389cd5f81b3475a2947ee4eb3e366a5
Dylan King <dwkblue51@yahoo.com>
2022-05-10 10:31:13 -0500

    write new test for PostForm

--- src/features/posts/PostForm.test.js
+++ src/features/posts/PostForm.test.js
@@ -3,14 +3,23 @@ import { PostForm } from "./PostForm";
 import userEvent from "@testing-library/user-event";
 import { render, screen } from "@testing-library/react";
 
+describe("PostForm", () => {
+  test("typing in textarea", async () => {
+    render(<PostForm />);
 
-describe("PostForm", ()=> {
-    test("typing in textarea", async ()=> {
-        render(<PostForm />);
-        // const user = userEvent;
-        const textarea = screen.getByRole('textbox');
-        await userEvent.type(textarea, 'test');
-        expect(textarea).toHaveValue('test');
-    });
-    test.todo("clicking X button hides ")
+    const textarea = screen.getByRole("textbox");
+    await userEvent.type(textarea, "test");
+    expect(textarea).toHaveValue("test");
+  });
+  test("clicking X button clears out content in textarea", async () => {
+    render(<PostForm />);
+
+    const textarea = screen.getByRole("textbox");
+    const xButton = screen.getByRole("button", { name: /x/i });
+
+    await userEvent.type(textarea, "test");
+    await userEvent.click(xButton);
+
+    expect(textarea).toHaveValue("");
+  });
 });
