53f0025aecec1b022470ba6257581954a1023c9a
Dylan King <dwkblue51@yahoo.com>
2022-05-10 13:50:50 -0500

    refactor

--- src/features/users/UserPhoto.js
+++ src/features/users/UserPhoto.js
@@ -1,11 +1,8 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { auth } from "../../firebase";
-import { useGetUsersQuery } from "../api/apiSlice";
 
 export const UserPhoto = () => {
   const user = auth.currentUser;
-  //   const { data: users } = useGetUsersQuery();
-  //   const currentUser = users.find((item) => item.id === user.id);
 
   return <img src={user.photoURL} alt="" />;
 };
