844bbced7453721005bccc539d96e04e2c6028ea
Dylan King <dwkblue51@yahoo.com>
2022-05-16 10:45:08 -0500

    comment out unused code

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -2,27 +2,27 @@ import { createApi, fakeBaseQuery } from "@reduxjs/toolkit/query/react";
 import { addDoc, collection, getDocs } from "firebase/firestore";
 import { db } from "../../firebase";
 
-const getPostsFromFirebase = async () => {
-  try {
-    let posts = [];
-    const request = await getDocs(collection(db, "posts"));
-    request.forEach((doc) => posts.push(doc.data()));
-    return { data: posts };
-  } catch (err) {
-    console.error(err);
-  }
-};
+// const getPostsFromFirebase = async () => {
+//   try {
+//     let posts = [];
+//     const request = await getDocs(collection(db, "posts"));
+//     request.forEach((doc) => posts.push(doc.data()));
+//     return { data: posts };
+//   } catch (err) {
+//     console.error(err);
+//   }
+// };
 
-const getUsers = async () => {
-  try {
-    let users = [];
-    const request = await getDocs(collection(db, "users"));
-    request.forEach((doc) => users.push(doc.data()));
-    return { data: users };
-  } catch (err) {
-    console.error(err);
-  }
-};
+// const getUsers = async () => {
+//   try {
+//     let users = [];
+//     const request = await getDocs(collection(db, "users"));
+//     request.forEach((doc) => users.push(doc.data()));
+//     return { data: users };
+//   } catch (err) {
+//     console.error(err);
+//   }
+// };
 
 export const apiSlice = createApi({
   baseQuery: fakeBaseQuery(),
