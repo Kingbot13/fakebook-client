8e772c49bf19f71736f06d0f001d3e1b8df6623c
Dylan King <dwkblue51@yahoo.com>
2022-05-18 15:01:58 -0500

    add positioning and color to SubmitBtn

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,16 @@
 import styled from "styled-components"
 
 export const SubmitBtn = styled.button`
-    background-color: #1877f2;
+    background-color: #1B74E4;
+    color: #fff;
+    border: none;
+    height: 36px;
+    border-radius: 6px;
+    width: 100%;
+    margin: 16px 16px 0;
+    font-weight: 600;
+    font-size: 0.9375rem;
+    &:hover {
+        background-color: #1877F2;
+    }
 `
