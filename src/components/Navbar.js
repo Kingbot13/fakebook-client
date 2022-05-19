ada930ecd46fdd8535af405171179cf1f2095a5b
Dylan King <dwkblue51@yahoo.com>
2022-05-19 10:36:20 -0500

    add Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from 'styled-components';
+
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div>link placeholder</div>
+        </nav>
+    )
+}
+
+export const StyledNav = styled(Navbar)`
+    background-color: blue;
+`
