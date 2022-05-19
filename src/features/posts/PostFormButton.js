1a5d5e5a082072bc3b4747f0539087e3feee1bdd
Dylan King <dwkblue51@yahoo.com>
2022-05-19 15:32:18 -0500

    add base styles to PostFormButton

--- src/features/posts/PostFormButton.js
+++ src/features/posts/PostFormButton.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { UserPhoto } from "../users/UserPhoto";
+import styles from '../../styles/PostFormButton.module.css';
 
 export const PostFormButton = ({ onClick }) => {
   return (
-    <div>
+    <div className={styles.container}>
       <UserPhoto />
       <button name="toggle-form" type="button" onClick={() => onClick()}>
         What's on your mind?
