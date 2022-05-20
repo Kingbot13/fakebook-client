c8ffe44796d6d26f2618fc17d40b1b4b42664d6a
Dylan King <dwkblue51@yahoo.com>
2022-05-20 09:35:12 -0500

    define width for PostList

--- src/features/posts/PostList.js
+++ src/features/posts/PostList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Post } from "./Post";
 import { useGetPostsQuery } from "../api/apiSlice";
+import styles from '../../styles/PostList.module.css';
 
 export const PostList = () => {
   const {data: posts} = useGetPostsQuery();
@@ -8,7 +9,7 @@ export const PostList = () => {
   
 
   return (
-    <div>
+    <div className={styles.container}>
       {posts ? posts.map(post => <Post key={post.id} name={post.data.name} content={post.data.content} photo={post.data.photo} />)
       :
       "Be the first to write a post!"}
