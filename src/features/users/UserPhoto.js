57bf616a89baa46f216724fac21eb613ffdedac5
Dylan King <dwkblue51@yahoo.com>
2022-05-20 08:23:51 -0500

    style UserPhoto

--- src/features/users/UserPhoto.js
+++ src/features/users/UserPhoto.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { auth } from "../../firebase";
+import styles from '../../styles/UserPhoto.module.css';
 
 export const UserPhoto = () => {
   const user = auth.currentUser;
 
-  return <img src={user.photoURL} alt="" />;
+  return <img className={styles.img} src={user.photoURL} alt="" />;
 };
