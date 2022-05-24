5e347c0700f7c55502fdcd99fe201c57385cb335
Dylan King <dwkblue51@yahoo.com>
2022-05-24 09:43:41 -0500

    Wrap App in BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import { StyledNav } from "./components/Navbar";
 import "./App.css";
 import { Newsfeed } from "./app/Newsfeed";
@@ -8,17 +8,19 @@ import { auth } from "./firebase";
 
 function App() {
   const [showNav, setShowNav] = useState(false);
+  const location = useLocation();
   useEffect(() => {
     if (auth.currentUser) setShowNav(true);
-  }, []);
+  }, [location]);
   return (
-    <Router>
+    <div>
       {showNav && <StyledNav />}
       <Routes>
         <Route path="/" element={<SignInPage />} />
         <Route path="/newsfeed" element={<Newsfeed />} />
       </Routes>
-    </Router>
+    </div>
+
   );
 }
 
