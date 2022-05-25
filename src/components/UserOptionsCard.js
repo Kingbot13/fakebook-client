547b4ca5ee0bd3ebb5ae3fa517dae22b7d5b4c3c
Dylan King <dwkblue51@yahoo.com>
2022-05-25 14:16:14 -0500

    add UserPhoto and user display name

--- src/components/UserOptionsCard.js
+++ src/components/UserOptionsCard.js
@@ -1,12 +1,26 @@
 import React from "react";
 import styles from '../styles/UserOptionsCard.module.css';
+import { UserPhoto } from "../features/users/UserPhoto";
+import { auth } from "../firebase";
 
 export const UserOptionsCard = () => {
     return (
         <div className={styles.main}>
             <div>
+                <div>
+                    <UserPhoto />
+                </div>
+                <div>
+                    <div>
+                        {auth.currentUser.displayName}
+                    </div>
+                    <div>
+                        see your profile
+                    </div>
+                </div>
                 user info stuff here
             </div>
+            <hr/>
             <ul>
                 <li>Log Out</li>
             </ul>
