4673967a895b882c84833837b8e54fcaf908ce08
Dylan King <dwkblue51@yahoo.com>
2022-05-27 15:05:59 -0500

    store photoURL in state

--- src/features/users/UserPhoto.js
+++ src/features/users/UserPhoto.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { auth } from "../../firebase";
 import styles from '../../styles/UserPhoto.module.css';
 
 export const UserPhoto = () => {
+  const [photoURL, setPhotoURL] = useState('');
   const user = auth.currentUser;
 
-  return <img className={styles.img} src={user.photoURL} alt="" />;
+  useEffect(() => {
+    setPhotoURL(user.photoURL);
+  }, []);
+
+  return <img className={styles.img} src={photoURL} alt="" />;
 };
