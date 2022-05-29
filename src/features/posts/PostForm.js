7e85412ee192570d09155ce69cda11296ef73456
Dylan King <dwkblue51@yahoo.com>
2022-05-29 16:14:43 -0500

    style and position header in PostForm

--- src/features/posts/PostForm.js
+++ src/features/posts/PostForm.js
@@ -2,12 +2,12 @@ import React from "react";
 import { UserPhoto } from "../users/UserPhoto";
 import { auth } from "../../firebase";
 import { SubmitBtn } from "../../components/Button";
-import styles from '../../styles/PostForm.module.css';
+import styles from "../../styles/PostForm.module.css";
 
 export const PostForm = ({ toggle, content, handleSubmit, handleChange }) => {
   return (
     <form className={styles.form}>
-      <div>
+      <div className={styles.headerContainer}>
         <h2>Create post</h2>
         <button type="button" onClick={toggle}>
           X
@@ -29,7 +29,11 @@ export const PostForm = ({ toggle, content, handleSubmit, handleChange }) => {
         <div>Add to your post</div>
         <div>icons</div>
       </div>
-      <SubmitBtn name="post-submit" type="button" onClick={(e) => handleSubmit(e)}>
+      <SubmitBtn
+        name="post-submit"
+        type="button"
+        onClick={(e) => handleSubmit(e)}
+      >
         Post
       </SubmitBtn>
     </form>
