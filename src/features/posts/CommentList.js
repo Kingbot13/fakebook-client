133e583c050537faf351618076f229d03f0d6c4a
Dylan King <dwkblue51@yahoo.com>
2022-06-20 22:01:05 -0500

    map comments array in CommentList

--- src/features/posts/CommentList.js
+++ src/features/posts/CommentList.js
@@ -1,1 +1,21 @@
 import React from "react";
+import { Comment } from "./Comment";
+import Proptypes from "prop-types";
+
+const CommentList = ({ comments }) => {
+  const mapComments = comments.map((comment) => {
+    return (
+      <Comment
+        key={comment.id}
+        content={comment.content}
+        userId={comment.userId}
+        id={comment.id}
+      />
+    );
+  });
+  return <div>{mapComments}</div>;
+};
+
+CommentList.propTypes = {
+  comments: Proptypes.array,
+};
