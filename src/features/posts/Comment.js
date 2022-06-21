327378212fefa025b15c4129af8ac81b7a5316e4
Dylan King <dwkblue51@yahoo.com>
2022-06-20 21:53:15 -0500

    add UserPhoto, name, and content to Comment

--- src/features/posts/Comment.js
+++ src/features/posts/Comment.js
@@ -1,1 +1,31 @@
 import React from "react";
+import { useGetUsersQuery } from "../api/apiSlice";
+import { UserPhoto } from "../users/UserPhoto";
+import Proptypes from "prop-types";
+
+const Comment = ({ userId, content, id }) => {
+  const { data: users } = useGetUsersQuery();
+  const foundUser = users.find((user) => user.id === userId);
+  const name = foundUser.data.name;
+  return (
+    <div>
+      <UserPhoto />
+      <div>
+        <div>
+          <div>
+            <div>{name}</div>
+            <p>{content}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Comment.propTypes = {
+  userId: Proptypes.string,
+  content: Proptypes.string,
+  id: Proptypes.string,
+};
+
+export { Comment };
