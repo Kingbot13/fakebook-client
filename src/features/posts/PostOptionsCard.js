6ff73a4540c3f84586011419c094f0284e9d5702
Dylan King <dwkblue51@yahoo.com>
2022-07-25 10:26:25 -0500

    add styles to PostOptionsCard

--- src/features/posts/PostOptionsCard.js
+++ src/features/posts/PostOptionsCard.js
@@ -4,8 +4,8 @@ import styles from '../../styles/PostOptionsCard.module.css';
 export const PostOptionsCard = ({toggleForm, deletePost}) => {
     return (
         <div className={styles.main}>
-            <div role='button' onClick={() => toggleForm()}>Edit post</div>
-            <div role='button' onClick={() => deletePost()}>Delete post</div>
+            <div className={styles.option} role='button' onClick={() => toggleForm()}>Edit post</div>
+            <div className={styles.option} role='button' onClick={() => deletePost()}>Delete post</div>
         </div>
     )
 }
